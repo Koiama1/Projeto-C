b547677879552f072c0750955b25a9292803990a
Koiama1 <114962890+Koiama1@users.noreply.github.com>
2023-10-17 18:51:17 -0300

    Criamos as funções de login e começamos a de novo cliente e funcionario

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "funcao.h"
+
+int main() {
+    // Implemente a lógica para carregar dados do cliente e as funções de débito, depósito, extrato e transferência.
+    // ...
+    struct Cliente clientes[100]; // Tamanho máximo para clientes
+    int numClientes = 0;
+
+    struct Funcionario funcionarios[100]; // Tamanho máximo para funcionários
+    int numFuncionarios = 0;
+
+    // Implemente a lógica para carregar dados de funcionários e clientes, bem como as funções de novo_funcionario, novo_cliente e login.
+    // ...
+
+    menu_login();
+
+
+    return 0;
+}
