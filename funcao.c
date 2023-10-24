59f5dc0cf7acfb32f90b420bc44b4a28d7d6e85c
Koiama1 <114962890+Koiama1@users.noreply.github.com>
2023-10-24 18:51:42 -0300

    Corrigimos algumas funções com erro e iniciamos a construção de outras funções que ainda não foram finalizadas

--- funcao.c
+++ funcao.c
@@ -1,150 +1,157 @@
-#include <stdio.h>
 #include "funcao.h"
 
-void debito() {
-    // Implemente a função de débito
-}
+struct funcionario {
+    char nome[100];
+    int cpf;
+    int senha;
+};
 
-void deposito() {
-    // Implemente a função de depósito
-}
+struct cliente {
+    int cnpj;
+    char razao_social[100];
+    char conta[10];
+    float saldo;
+    int senha;
+};
 
-void extrato() {
-    // Implemente a função de extrato
-}
+struct funcionario funcionarios[100];
+struct cliente clientes[100];
 
-void transferencia() {
-    // Implemente a função de transferência
-}
+// Função para cadastrar um novo funcionário
+void novo_funcionario(void) {
+    char nome[100];
+    int cpf;
+    int senha;
 
-void menu_cliente() {
-    int menu;
+    printf("Digite seu nome: ");
+    scanf("%s", nome);
+    printf("Digite seu CPF: ");
+    scanf("%d", &cpf);
+    printf("Digite sua senha: ");
+    scanf("%d", &senha);
 
-    while (1) {
-        printf("1. Débito\n");
-        printf("2. Depósito\n");
-        printf("3. Extrato\n");
-        printf("4. Transferência entre contas\n");
-        printf("5. Sair\n");
-        printf("Digite o número respectivo à opção desejada: ");
-        scanf("%d", &menu);
-
-        if (menu == 5) {
-            break;
-        } else if (menu == 1) {
-            debito();
-        } else if (menu == 2) {
-            deposito();
-        } else if (menu == 3) {
-            extrato();
-        } else if (menu == 4) {
-            transferencia();
+    // Verifica se o funcionário já existe
+    for (int i = 0; i < sizeof(funcionarios) / sizeof(funcionarios[0]); i++) {
+        if (funcionarios[i].cpf == cpf) {
+            printf("Funcionario ja cadastrado.\n");
+            return;
         }
     }
-}
-void apagar_funcionario() {
-    // Implemente a função de apagar funcionário
-}
 
-void listar_funcionarios() {
-    // Implemente a função de listar funcionários
-}
+    // Adiciona o funcionário ao vetor
+    struct funcionario novo_funcionario = {nome, cpf, senha};
+    funcionarios[sizeof(funcionarios) / sizeof(funcionarios[0])] = novo_funcionario;
 
-void listar_clientes(){
-
+    printf("Funcionario cadastrado com sucesso!\n");
+    menu_login();
 }
 
-void apagar_cliente() {
-
-}
-
-void menu_funcionario() {
-    int menu;
-
-    while (1) {
-        printf("1. Apagar cliente\n");
-        printf("2. Listar clientes\n");
-        printf("3. Apagar funcionário\n");
-        printf("4. Listar funcionários\n");
-        printf("5. Sair\n");
-        printf("Digite o número respectivo à opção desejada: ");
-        scanf("%d", &menu);
-
-        if (menu == 5) {
-            break;
-        } else if (menu == 1) {
-            apagar_cliente();
-        } else if (menu == 2) {
-            listar_clientes();
-        } else if (menu == 3) {
-            apagar_funcionario();
-        } else if (menu == 4) {
-            listar_funcionarios();
-        }
-    }
-}
-
-void novo_funcionario() {
-    // Implemente a função novo_funcionario
-}
-
-void novo_cliente() {
+// Função para cadastrar um novo cliente
+void novo_cliente(void) {
     int cnpj;
     char razao_social[100];
     char conta[10];
     float saldo;
     int senha;
 
-    printf("CNPJ: ");
+    printf("Digite seu CNPJ: ");
     scanf("%d", &cnpj);
+    printf("Digite sua razao social: ");
+    scanf("%s", razao_social);
+    printf("Digite o tipo da conta (Comum ou Plus): ");
+    scanf("%s", conta);
+    printf("Digite o saldo inicial: ");
+    scanf("%f", &saldo);
+    printf("Digite sua senha: ");
+    scanf("%d", &senha);
 
-    for (int i = 0; i < numClientes; i++) {
+    // Verifica se o cliente já existe
+    for (int i = 0; i < sizeof(clientes) / sizeof(clientes[0]); i++) {
         if (clientes[i].cnpj == cnpj) {
-            printf("Cliente já cadastrado\n");
-            menu_login();
+            printf("Cliente ja cadastrado.\n");
             return;
         }
     }
 
-    printf("Razão Social: ");
-    scanf("%s", razao_social);
-
-    printf("Tipo de conta (Comum ou Plus): ");
-    scanf("%s", conta);
-
-    if (strcmp(conta, "Plus") != 0 && strcmp(conta, "Comum") != 0) {
-        printf("Tipo de conta inválido\n");
-        menu_login();
-        return;
-    }
-
-    printf("Digite o valor inicial da conta: ");
-    scanf("%f", &saldo);
-
-    printf("Defina uma senha: ");
-    scanf("%d", &senha);
-
-    struct Cliente novoCliente;
-    novoCliente.cnpj = cnpj;
-    strcpy(novoCliente.razao_social, razao_social);
-    strcpy(novoCliente.conta, conta);
-    novoCliente.saldo = saldo;
-    novoCliente.senha = senha;
-
-    clientes[numClientes] = novoCliente;
-    numClientes++;
+    // Adiciona o cliente ao vetor
+    struct cliente novo_cliente = {cnpj, razao_social, conta, saldo, senha};
+    clientes[sizeof(clientes) / sizeof(clientes[0])] = novo_cliente;
 
     printf("Cliente cadastrado com sucesso!\n");
     menu_login();
-
 }
 
-void login() {
-    // Implemente a função de login
+void menu_cliente(){
+  //Função para login cliente
 }
 
-void sair() {
-    // Implemente a função de sair
+void menu_funcionario(){
+  //Função para login funcionario
+}
+
+void login(void) {
+    // Obtém o tipo de login
+    int tipo_de_login;
+    printf("Qual login deseja acessar? (Funcionario = 1) (cliente = 2):  ");
+    scanf("%d", &tipo_de_login);
+
+    // Verifica o tipo de login
+    if (tipo_de_login == 1) {
+        // Login de funcionário
+
+        // Obtém o CPF do funcionário
+        int cpf;
+        printf("Digite seu CPF: ");
+        scanf("%d", &cpf);
+
+        // Verifica se o funcionário existe
+        for (int i = 0; i < sizeof(funcionarios) / sizeof(funcionarios[0]); i++) {
+            if (funcionarios[i].cpf == cpf) {
+                // Obtém a senha do funcionário
+                int senha;
+                printf("Digite sua senha: ");
+                scanf("%d", &senha);
+
+                // Verifica se a senha está correta
+                if (funcionarios[i].senha == senha) {
+                    // Login bem-sucedido
+                    menu_funcionario();
+                }
+            }
+        }
+
+        // Login inválido
+        return 0;
+    } else if (tipo_de_login == 2) {
+        // Login de cliente
+
+        // Obtém o CNPJ do cliente
+        int cnpj;
+        printf("Digite seu CNPJ: ");
+        scanf("%d", &cnpj);
+
+        // Verifica se o cliente existe
+        for (int i = 0; i < sizeof(clientes) / sizeof(clientes[0]); i++) {
+            if (clientes[i].cnpj == cnpj) {
+                // Obtém a senha do cliente
+                int senha;
+                printf("Digite sua senha: ");
+                scanf("%d", &senha);
+
+                // Verifica se a senha está correta
+                if (clientes[i].senha == senha) {
+                    // Login bem-sucedido
+                    menu_cliente();
+                }
+            }
+        }
+
+        // Login inválido
+        return 0;
+    } else {
+        // Opção inválida
+        return 0;
+    }
 }
 
 void menu_login() {
@@ -159,7 +166,6 @@ void menu_login() {
         scanf("%d", &menu);
 
         if (menu == 4) {
-            sair();
             break;
         } else if (menu == 1) {
             novo_funcionario();
