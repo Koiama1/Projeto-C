7e3e9224d3307615609ec3f6c86b9e4f95961c83
Koiama1 <114962890+Koiama1@users.noreply.github.com>
2023-10-24 18:49:17 -0300

    Alterações no main.c para correções

--- main.c
+++ main.c
@@ -1,20 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "funcao.h"
 
+int tipo_de_login;
+
 int main() {
-    // Implemente a lógica para carregar dados do cliente e as funções de débito, depósito, extrato e transferência.
-    // ...
-    struct Cliente clientes[100]; // Tamanho máximo para clientes
-    int numClientes = 0;
-
-    struct Funcionario funcionarios[100]; // Tamanho máximo para funcionários
-    int numFuncionarios = 0;
-
-    // Implemente a lógica para carregar dados de funcionários e clientes, bem como as funções de novo_funcionario, novo_cliente e login.
-    // ...
-
-    menu_login();
-
-
-    return 0;
+  menu_login();
 }
