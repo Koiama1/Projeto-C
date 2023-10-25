1cb7943003c3aea7f94afc48316f3d822ce14a75
Koiama1 <114962890+Koiama1@users.noreply.github.com>
2023-10-25 17:12:03 -0300

    Adicionamos as structs de clientes e funcionarios e modificamos a função apagar clientes

--- funcao.h
+++ funcao.h
@@ -4,19 +4,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct Cliente{
+    int cnpj;
+    char razao_social[100];
+    char conta[10];
+    float saldo;
+    int senha;
+};
+
+struct Funcionario {
+    int cpf;
+    char nome[100];
+    int senha;
+};
+
 void novo_funcionario(void);
 void novo_cliente(void);
 void login(void);
-void apagar_cliente(void);
+void apagar_clientes(struct Cliente *clientes, int tamanho);
 void listar_clientes(void);
-void apagar_funcionario(void);
+void apagar_funcionarios(void);
 void listar_funcionarios(void);
 void debito(void);
 void deposito(void);
 void extrato(void);
 void transferencia(void);
-void menu_login();
-void menu_funcionario();
-void menu_cliente();
+void menu_login(void);
+void menu_funcionario(void);
+void menu_cliente(void);
 
 #endif
